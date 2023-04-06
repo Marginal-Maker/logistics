35e2451cf7eff960ea8c65fd9aaf1aaeea43291d
Marginal-Maker <517954901@qq.com>
2023-04-06 20:50:57 +0800

    test

---
+++ logistics-parent/logistics-manager/logistics-manager-web/src/main/java/test.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @descriotion
+ * @auther majing
+ * @data 2023/04/03/19:08
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+
+}
